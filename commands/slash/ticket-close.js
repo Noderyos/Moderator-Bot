f86670e2b985dcccd942949ee0cd48720adc0ab1
Arianne <76917463+ariannelafraise@users.noreply.github.com>
2026-08-15 11:29:36 -0400

    logs

--- commands/slash/ticket-close.js
+++ commands/slash/ticket-close.js
@@ -1,7 +1,8 @@
 /*
  * author : Mizari (Mizari-Dev)
  */
-const { ApplicationCommandType, PermissionsBitField } = require("discord.js");
+const { ApplicationCommandType, PermissionsBitField, ApplicationCommandOptionType, EmbedBuilder } = require("discord.js");
+const discordTranscripts = require('discord-html-transcripts');
 const ticketConfig = require('../../configuration/tickets.json');
 
 module.exports = {
@@ -10,6 +11,15 @@ module.exports = {
       descriptionLocalizations: {fr: "Fermer le ticket."},
     type: ApplicationCommandType.ChatInput,
     defaultMemberPermissions: PermissionsBitField.Flags.ManageChannels,
+    options: [
+        {
+            name: "reason",
+            description: "The reason you're closing the ticket.",
+            descriptionLocalizations: {fr: "La raison pour laquelle vous fermez le ticket."},
+            type: ApplicationCommandOptionType.String,
+            required: false,
+        }
+    ],
     /**
      *
      * @param {Client} client
@@ -22,6 +32,39 @@ module.exports = {
             return;
         }
         await interaction.deferReply({ ephemeral: true });
+
+        const ticketId = interaction.channel.name.split("-")[1];
+
+        let transcriptFile;
+        try {
+            transcriptFile = await discordTranscripts.createTranscript(interaction.channel, {
+                fileName: `ticket-${ticketId}.html`
+            });
+        } catch (err) {
+            console.error('Error generating transcript:', err);
+        }
+
+        const logsChannel = client.channels.cache.get(ticketConfig.logsChannel);
+        if (logsChannel) {
+            const embedLog = new EmbedBuilder()
+                .setTitle(`Ticket #${ticketId} fermé`)
+                .setColor('#ff0000')
+                .addFields(
+                    { name: 'Raison de fermeture', value: args[0] ?? "Aucune raison donnée", inline: false },
+                )
+
+            if (transcriptFile) {
+                await logsChannel.send({
+                    embeds: [embedLog],
+                    files: [transcriptFile]
+                });
+            } else {
+                await logsChannel.send({
+                    embeds: [embedLog]
+                });
+            }
+        }
+
         await interaction.channel.delete();
     },
 };
